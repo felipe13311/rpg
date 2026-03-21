b94dce42b4dec59dd370a1e673dc6fc902b0e295
felipe13311 <146225214+felipe13311@users.noreply.github.com>
2026-03-21 17:55:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,17 +7,18 @@ app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
     console.log("🩸 Novo Agente entrou na Facility...");
-    
+
     socket.on("roll", (data) => {
         socket.broadcast.emit("roll", data);
     });
-    
+
     socket.on("ataque", (dano) => {
         socket.broadcast.emit("receberAtaque", dano);
     });
 });
 
-http.listen(3000, () => {
-    console.log("🌕 BLOOD MOON VTT RODANDO EM http://localhost:3000");
-    console.log("Estilo Ordem Paranormal × Lobotomy Corporation ATIVADO");
+// 🔥 PORTA MÁGICA DO RAILWAY
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log(`🌕 BLOOD MOON VTT ONLINE NA PORTA ${PORT} - Facility da Ordem ativada!`);
 });
