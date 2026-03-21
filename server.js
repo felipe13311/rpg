c1b8e04933bc4db46a7b3002416d1ea2c1643007
felipe13311 <146225214+felipe13311@users.noreply.github.com>
2026-03-21 18:04:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,24 @@ const io = require("socket.io")(http);
 
 app.use(express.static(__dirname));
 
+// Health check pro Railway (resolve SIGTERM e "Stopping Container")
+app.get('/', (req, res) => {
+  res.send('🌕 BLOOD MOON VTT ONLINE - Facility da Ordem ativa!');
+});
+
 io.on("connection", (socket) => {
     console.log("🩸 Novo Agente entrou na Facility...");
 
     socket.on("roll", (data) => {
         socket.broadcast.emit("roll", data);
     });
+
+    socket.on("ataque", (dano) => {
+        socket.broadcast.emit("receberAtaque", dano);
+    });
 });
 
-// 🔥 LINHA OBRIGATÓRIA PRO RAILWAY
+// Porta dinâmica (já tá certa)
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`🌕 BLOOD MOON VTT ONLINE NA PORTA ${PORT} - Facility da Ordem ativada!`);
