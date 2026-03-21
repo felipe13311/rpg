37c5456343b1ad054b3add8d1a09299a2cbead66
felipe13311 <146225214+felipe13311@users.noreply.github.com>
2026-03-21 17:49:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+    console.log("🩸 Novo Agente entrou na Facility...");
+    
+    socket.on("roll", (data) => {
+        socket.broadcast.emit("roll", data);
+    });
+    
+    socket.on("ataque", (dano) => {
+        socket.broadcast.emit("receberAtaque", dano);
+    });
+});
+
+http.listen(3000, () => {
+    console.log("🌕 BLOOD MOON VTT RODANDO EM http://localhost:3000");
+    console.log("Estilo Ordem Paranormal × Lobotomy Corporation ATIVADO");
+});
